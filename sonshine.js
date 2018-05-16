5fed937ef6b5594ce90b96b994eb8d88011feab9
Shane Folden <sfolden@umail.ucsb.edu>
2018-05-16 01:34:09 -0700

    made history timeline blurbs

--- sonshine.js
+++ sonshine.js
@@ -19,11 +19,42 @@ function callbackFunc() {
 
     if (isElementInViewport(items[i])) {
       items[i].classList.add("in-view");
-      
-
     }
   }
 }
 
 window.addEventListener("load", callbackFunc);
 window.addEventListener("scroll", callbackFunc);
+
+var times = document.getElementsByClassName("timeElement");
+/*
+for (var j = 0; j < items.length; j++) {
+  if(items[j].offsetHeight>300 && j!=5)
+  {
+     console.log("exactly");
+     times[j].classList.add("setHeight");
+  }
+
+}
+
+for (var k = 0; k < items.length; k++) {
+  items[j].onmouseover = function(){
+    if ( $( times[j] ).hasClass( "setHeight" ) ) {
+      alert(k);
+    }
+  };
+}
+*/
+setTimeout(function()
+{
+	var max = 500;
+  var tot, str;
+  $('.timeElement').each(function() {
+  	str = String($(this).html());
+  	tot = str.length;
+    str = (tot <= max)
+    	? str
+      : str.substring(0,(max + 1))+"...";
+    $(this).html(str);
+  });
+},500);
