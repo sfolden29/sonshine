eebb4c04fbd1af56a20b790f76409479a6c03894
supercarlos297 <footballshane@gmail.com>
2018-10-23 20:10:10 -0700

    js stuff

---
+++ sonshine2.js
@@ -0,0 +1,16 @@
+fixPad();
+function fixPad(){
+  
+
+  var screenHeight = screen.height;
+  var clientHeight = document.getElementById('indexH1').clientHeight;
+  var clientHeight2 = document.getElementById('indexH2').clientHeight;
+  var h1MarginTop = parseInt( $("#indexH1").css("marginTop") );
+  var h1MarginBottom = parseInt( $("#indexH1").css("marginBottom") );
+
+  var divHeights = clientHeight+clientHeight2+h1MarginTop+h1MarginBottom;
+
+  var topmar = screenHeight-divHeights;
+  document.getElementById("jsgod").style.marginTop = topmar+"px";
+
+}
